5485080d57864ce0633787fd5ab35bdc841b99a3
Artur Akulich <>
2024-09-21 23:43:44 +0300

    Initial commit

---
+++ ToDos/ToDos/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ToDos
+//
+//  Created by Artur Akulich on 21.09.24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ ToDos/ToDos/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  ToDos
+//
+//  Created by Artur Akulich on 21.09.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let mainViewController = MainViewController()
+        self.window?.rootViewController = mainViewController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
